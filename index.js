96b9365f1a967ebc763687fa96ae057c694ee4cf
CahenTaylor <cahentaylor@gmail.com>
2023-03-27 12:04:07 -0600

    saving for lecture

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+// fetch("http://localhost:3000/dogs", newPup); {
+//     const newPup = {method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle",
+//     }),
+// // }
+
+// };
+// fetch("http://localhost:3000/dogs", newPup);
+
+function submitData(userName, userEmail) {
+    fetch("http://localhost:3000/users", newUser); {
+        const newUser = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                name: userName,
+                email: userEmail,
+            }),
+        }
+        console.log(newUser);
+    }
+    return fetch("http://localhost:3000/users", newUser);
+}
