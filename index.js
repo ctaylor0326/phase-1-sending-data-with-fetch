425304074e5531420df6f8c699bdb447e8474278
CahenTaylor <cahentaylor@gmail.com>
2023-03-27 15:49:33 -0600

    finished

--- index.js
+++ index.js
@@ -14,20 +14,37 @@
 // };
 // fetch("http://localhost:3000/dogs", newPup);
 
-function submitData(userName, userEmail) {
-    fetch("http://localhost:3000/users", newUser); {
-        const newUser = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            },
-            body: JSON.stringify({
-                name: userName,
-                email: userEmail,
-            }),
-        }
-        console.log(newUser);
-    }
-    return fetch("http://localhost:3000/users", newUser);
+// const userInfo = {
+//   name: "Name",
+//   email: "Email",
+// };
+
+// const newUser = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+
+//   body: JSON.stringify({ userInfo }),
+// };
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name, email }),
+  })
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(object){
+    document.body.innerHTML=object["id"]
+  })
+  .catch(function(error){
+    document.body.innerHTML=error.message
+  })
+  
 }
